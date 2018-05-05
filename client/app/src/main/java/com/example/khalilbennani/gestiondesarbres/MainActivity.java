7e7cd5dcaa484f53e404ce2cbbac8ea2fdd0537f
mouunia <mouunianordine@gmail.com>
2018-05-04 23:18:24 -0400

    interface ..

--- client/app/src/main/java/com/example/khalilbennani/gestiondesarbres/MainActivity.java
+++ client/app/src/main/java/com/example/khalilbennani/gestiondesarbres/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.khalilbennani.gestiondesarbres;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox rememberMe;
+    private CheckBox rememberPassword;
+    private Button buttonLogin;
+    private EditText editPassword;
+    private EditText editUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPassword = (EditText) findViewById(R.id.adresseIP);
+        editUsername = (EditText) findViewById(R.id.username);
+        rememberMe = (CheckBox) findViewById(R.id.rememberMe);
+        rememberPassword = (CheckBox) findViewById(R.id.rememberPassword);
+
+
     }
 }
