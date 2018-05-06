8dd8ce1bcc07604bf55a66b244e1a75451398832
Khalil Bennani <khalil.bennani@polymtl.ca>
2018-05-06 09:14:53 -0400

    page comment + boutton exit and save

--- Client/app/src/main/java/com/example/khalilbennani/gestiondesarbres/ActionActivity.java
+++ Client/app/src/main/java/com/example/khalilbennani/gestiondesarbres/ActionActivity.java
@@ -2,6 +2,7 @@ package com.example.khalilbennani.gestiondesarbres;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActionActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class ActionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
+    }
+
+    public void confirmer(View v){
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 }
